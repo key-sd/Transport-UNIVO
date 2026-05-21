4bc1c515cc0d3fb8762583ad1228f2d99c70001f
Ale <emelymunoz672@gmail.com>
2026-05-21 10:56:28 -0600

    refeactor: Extiende los dígitos del codigo universitario a 9 en vez de 8 (u20260001)

--- db/insertar-datos.sql
+++ db/insertar-datos.sql
@@ -1,9 +1,9 @@
 use transport_univo;
 
 INSERT INTO `usuarios` (`codigo_universitario`, `password_hash`, `rol_id`) VALUES 
-('u2026001','$2y$10$vqyXr8as1lK//K.JQF3Q2uxC9h4ivZl4Ijj.FfJfcyw.aCIqDA1Pi', 1), 
-('u2026002','$2y$10$kmxPL1U7kDc59MM71rr4rO5yQOyJAfxYpaViQglHvXZ9kNrmOcN8G', 2), 
-('u2026003','$2y$10$LXz7KvcADrt8rxvU2wk3Ye0Sqd.Se0hhmRD1CAxf6g3tpN5/MwyFG', 3);
+('u20260001','$2y$10$vqyXr8as1lK//K.JQF3Q2uxC9h4ivZl4Ijj.FfJfcyw.aCIqDA1Pi', 1), 
+('u20260002','$2y$10$kmxPL1U7kDc59MM71rr4rO5yQOyJAfxYpaViQglHvXZ9kNrmOcN8G', 2), 
+('u20260003','$2y$10$LXz7KvcADrt8rxvU2wk3Ye0Sqd.Se0hhmRD1CAxf6g3tpN5/MwyFG', 3);
 
 INSERT INTO `conductores` (`usuario_id`, `nombre`, `apellido`, `telefono`) VALUES 
 (3, 'Juan', 'Pérez', '555-1234');
--- db/script.sql
+++ db/script.sql
@@ -88,9 +88,9 @@ INSERT INTO `roles` (`nombre`) VALUES
 
 -- Insertar usuarios 
 INSERT INTO `usuarios` (`codigo_universitario`, `password_hash`, `rol_id`) VALUES 
-('u2026001','$2y$10$vqyXr8as1lK//K.JQF3Q2uxC9h4ivZl4Ijj.FfJfcyw.aCIqDA1Pi', 1), 
-('u2026002','$2y$10$kmxPL1U7kDc59MM71rr4rO5yQOyJAfxYpaViQglHvXZ9kNrmOcN8G', 2), 
-('u2026003','$2y$10$LXz7KvcADrt8rxvU2wk3Ye0Sqd.Se0hhmRD1CAxf6g3tpN5/MwyFG', 3);
+('u20260001','$2y$10$vqyXr8as1lK//K.JQF3Q2uxC9h4ivZl4Ijj.FfJfcyw.aCIqDA1Pi', 1), 
+('u20260002','$2y$10$kmxPL1U7kDc59MM71rr4rO5yQOyJAfxYpaViQglHvXZ9kNrmOcN8G', 2), 
+('u20260003','$2y$10$LXz7KvcADrt8rxvU2wk3Ye0Sqd.Se0hhmRD1CAxf6g3tpN5/MwyFG', 3);
 
 -- Insertar conductor 
 INSERT INTO `conductores` (`usuario_id`, `nombre`, `apellido`, `telefono`) VALUES 
