0a49394caf9f38b360906a916e11dcd80344ac47
Key <keysanchez706@gmail.com>
2026-06-10 22:10:24 -0600

    cambios para despliegue

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,15 +1,17 @@
 <?php
-$server = "localhost";
-$user = "root";
-$pass = "";
-$db = "db_transport_univo";
+$server = "sql5.freesqldatabase.com";
+$user = "sql5830068";
+$pass = "4IKjt9vqYt";
+$db = "sql5830068";
+$port = 3306;
 
 date_default_timezone_set('America/El_Salvador');
-$conexion = new mysqli($server, $user, $pass, $db);
+
+$conexion = new mysqli($server, $user, $pass, $db, $port);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
 
-$conn = $conexion; 
+$conn = $conexion;
 ?>
